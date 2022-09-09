0d249cafa7c9484cbc97306fe768afd1b759e74c
aldinh777 <aldi.nh789@gmail.com>
2022-09-09 13:36:28 +0800

    fix children preprocessor

--- parser/preprocess/preprocess-children.ts
+++ parser/preprocess/preprocess-children.ts
@@ -4,6 +4,7 @@ import { Identifiers } from '../extractParams';
 export default function (item: CMLObject, [dep]: Identifiers): CMLObject {
     const { tag } = item;
     if (tag === 'children') {
+        item.tag = 'Children';
         dep.push('Children');
     }
     return item;
