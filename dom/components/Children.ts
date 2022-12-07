5b9a9ceea34e78e2ba0aec0801f6a8fcc69bffea
aldinh777 <aldi.nh789@gmail.com>
2022-12-07 16:29:02 +0800

    something idk im sleepy

--- dom/components/Children.ts
+++ dom/components/Children.ts
@@ -4,13 +4,12 @@ import { propAlias, readAlias } from '../prop-util';
 
 export default function (props: Properties = {}, context?: Context): NodeComponent[] | void {
     if (context && context._super) {
-        const { children, extracts, params, _super } = context._super;
+        const { children, params, _super } = context._super;
         const prevSlotName = _super.slotname;
         _super.slotname = props.name;
         const exported = typeof props.export === 'string' ? readAlias(props.export) : [];
         const localParams = propAlias(params, exported, context.params);
-        const childrenParams = propAlias(params, extracts, localParams);
-        const output = intoDom(children, childrenParams, _super);
+        const output = intoDom(children, localParams, _super);
         _super.slotname = prevSlotName;
         return output;
     }
