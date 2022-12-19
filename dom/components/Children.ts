02cd1a985b431c9518f3b452c4c604cb8dccfc50
aldinh777 <aldi.nh789@gmail.com>
2022-12-19 12:59:12 +0800

    slot fix #3

--- dom/components/Children.ts
+++ dom/components/Children.ts
@@ -6,14 +6,14 @@ export default function (props: Properties = {}, context?: Context): NodeCompone
     if (context && context._super) {
         const { children, extracts, params, _super } = context._super;
         const prevSlotName = _super?.slotname;
-        if (_super && prevSlotName) {
+        if (_super) {
             _super.slotname = props.name;
         }
         const exported = typeof props.export === 'string' ? readAlias(props.export) : [];
         const localParams = propAlias(params, exported, context.params);
         const childrenParams = propAlias(localParams, extracts, localParams);
         const output = intoDom(children, childrenParams, _super);
-        if (_super && prevSlotName) {
+        if (_super) {
             _super.slotname = prevSlotName;
         }
         return output.length ? output : intoDom(context.children, context.params, context._super);
