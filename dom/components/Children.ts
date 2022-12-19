1e1e295ee3acf102a0d9d9c6721315c9656c2734
aldinh777 <aldi.nh789@gmail.com>
2022-12-19 12:25:52 +0800

    fix _super in children

--- dom/components/Children.ts
+++ dom/components/Children.ts
@@ -6,12 +6,16 @@ export default function (props: Properties = {}, context?: Context): NodeCompone
     if (context && context._super) {
         const { children, extracts, params, _super } = context._super;
         const prevSlotName = _super.slotname;
-        _super.slotname = props.name;
+        if (_super) {
+            _super.slotname = props.name;
+        }
         const exported = typeof props.export === 'string' ? readAlias(props.export) : [];
         const localParams = propAlias(params, exported, context.params);
         const childrenParams = propAlias(localParams, extracts, localParams);
         const output = intoDom(children, childrenParams, _super);
-        _super.slotname = prevSlotName;
+        if (_super) {
+            _super.slotname = prevSlotName;
+        }
         return output.length ? output : intoDom(context.children, context.params, context._super);
     }
 }
