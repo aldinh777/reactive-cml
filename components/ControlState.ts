1cc1cd24fb29f11e826a4449edf0915e2b3faa03
aldinh777 <aldi.nh789@gmail.com>
2022-12-31 09:18:49 +0800

    fix thing brokne

--- components/ControlState.ts
+++ components/ControlState.ts
@@ -43,14 +43,25 @@ export default function (props: Properties = {}, context?: Context): NodeCompone
     if (stateValue.getValue()) {
         component.items = elements;
         const mountHandlers = [];
+        const dismountHandlers = [];
         for (const elem of elements) {
-            if (!(elem instanceof Node) && elem.onMount) {
-                mountHandlers.push(elem.onMount);
+            if (!(elem instanceof Node)) {
+                if (elem.onMount) {
+                    mountHandlers.push(elem.onMount);
+                }
+                if (elem.onDismount) {
+                    dismountHandlers.push(elem.onDismount);
+                }
             }
         }
         component.onMount = function () {
             for (const mountHandler of mountHandlers) {
                 mountHandler();
+            }
+        };
+        component.onDismount = function () {
+            for (const dismountHandler of dismountHandlers) {
+                dismountHandler();
             }
         };
     } else {
