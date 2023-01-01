8061164092ccefaa4868d85fdf4f3f005851e4da
aldinh777 <aldi.nh789@gmail.com>
2023-01-02 05:07:18 +0800

    dom-util minification

--- dom/dom-util.ts
+++ dom/dom-util.ts
@@ -1,76 +1,62 @@
 import { Context, NodeComponent } from '.';
 import { RCResult, Component } from '../src';
 
+type ComponentHandler = (target: Node, components: NodeComponent[], nodeBefore: Node) => void;
+
 export const _doc: Document = document;
 
 export const _text = (text: string): Text => _doc.createTextNode(text);
 export const _elem = (tag: string): HTMLElement => _doc.createElement(tag);
 
-export function mount(target: null | Node, components: NodeComponent[], nodeBefore?: Node) {
-    if (target) {
-        append(target, components, nodeBefore);
-    }
-    for (const component of components) {
-        if (!(component instanceof Node)) {
-            if (component.mount) {
-                component.mount();
-            }
-            mount(null, component.items);
-        }
-    }
-}
-
-export function dismount(target: null | Node, components: NodeComponent[]) {
-    if (target) {
-        remove(target, components);
-    }
-    for (const component of components) {
-        if (!(component instanceof Node)) {
-            if (component.dismount) {
-                component.dismount();
-            }
-            dismount(null, component.items);
-        }
-    }
-}
-
-export function append(parentNode: Node, components: NodeComponent[], nodeBefore?: Node) {
-    for (const component of components) {
-        const isNode = component instanceof Node;
-        const item = isNode ? component : component.root;
-        if (isNode || item) {
-            if (nodeBefore) {
-                parentNode.insertBefore(item, nodeBefore);
+const _rec = (nodeHandler: (parent: Node, item: Node, nodeBefore?: Node) => void) => {
+    const recurse = (parentNode: Node, components: NodeComponent[], nodeBefore?: Node) => {
+        for (const component of components) {
+            const isNode = component instanceof Node;
+            const item = isNode ? component : component.root;
+            if (isNode || item) {
+                nodeHandler(parentNode, item, nodeBefore);
             } else {
-                parentNode.appendChild(item);
+                recurse(parentNode, component.items, nodeBefore);
             }
-        } else {
-            append(parentNode, component.items, nodeBefore);
         }
+    };
+    return recurse;
+};
+export const append = _rec((parentNode, item, nodeBefore) => {
+    if (nodeBefore) {
+        parentNode.insertBefore(item, nodeBefore);
+    } else {
+        parentNode.appendChild(item);
     }
-}
+});
+export const remove = _rec((parentNode, item) => parentNode.removeChild(item));
 
-export function remove(parentNode: Node, components: NodeComponent[]) {
-    for (const component of components) {
-        const isNode = component instanceof Node;
-        const item = isNode ? component : component.root;
-        if (isNode || item) {
-            parentNode.removeChild(item);
-        } else {
-            remove(parentNode, component.items);
+const _recMount = (executor: ComponentHandler, caller: string) => {
+    const recurseMount = (target: null | Node, components: NodeComponent[], nodeBefore?: Node) => {
+        if (target) {
+            executor(target, components, nodeBefore);
         }
-    }
-}
+        for (const component of components) {
+            if (!(component instanceof Node)) {
+                component[caller]?.();
+                recurseMount(null, component.items);
+            }
+        }
+    };
+    return recurseMount;
+};
+export const mount = _recMount(append, 'mount');
+export const dismount = _recMount(remove, 'dismount');
 
-export function setAttr(element: HTMLElement, attribute: string, value: any) {
+export const setAttr = (element: HTMLElement, attribute: string, value: any) => {
     if (element.hasAttribute(attribute)) {
         element.setAttribute(attribute, value);
-        return;
+    } else {
+        const att = _doc.createAttribute(attribute);
+        att.value = value;
+        element.setAttributeNode(att);
     }
-    const att = _doc.createAttribute(attribute);
-    att.value = value;
-    element.setAttributeNode(att);
-}
+};
 
 export function simpleDom(tree: RCResult[], component?: Context): NodeComponent[] {
     const result: NodeComponent[] = [];
@@ -79,16 +65,16 @@ export function simpleDom(tree: RCResult[], component?: Context): NodeComponent[
     for (const item of tree) {
         if (typeof item === 'string') {
             result.push(_text(item));
-            continue;
+        } else {
+            const [tag, props, , children] = item as Component;
+            const elem = _elem(tag);
+            for (const prop in props) {
+                const value = props[prop];
+                setAttr(elem, prop, value);
+            }
+            append(elem, simpleDom(children));
+            result.push(elem);
         }
-        const [tag, props, , children] = item as Component;
-        const elem = _elem(tag);
-        for (const prop in props) {
-            const value = props[prop];
-            setAttr(elem, prop, value);
-        }
-        append(elem, simpleDom(children));
-        result.push(elem);
     }
     return onMount || onDismount
         ? [{ items: result, mount: onMount, dismount: onDismount }]
